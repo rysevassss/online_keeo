20d9d10bfe510712b147b88fcf37d89dd29d7324
rysevassss <hate.tiktok@mail.ru>
2024-06-13 23:21:21 +0700

    promor

--- backend/routes/PromoR.js
+++ backend/routes/PromoR.js
@@ -1,10 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const promoController = require('../controllers/PromoC')
-const checkRole = require("../middleware/checkRole");
+const checkRole = require('../middleware/CheckRole.js');
 
 router.post('/', checkRole('admin'), promoController.create)
 router.get('/', promoController.getAll)
-router.delete('/:id',checkRole('admin'), promoController.delete)
+router.delete('/:id', checkRole('admin'), promoController.delete)
 
 module.exports = router
