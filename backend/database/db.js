dd5038f8bdd6da4096ac6ef6f163252ed1e43990
rysevassss <hate.tiktok@mail.ru>
2024-06-16 21:18:38 +0700

    newdb

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres://postgres:8888@localhost:5432/keeo', {
+const sequelize = new Sequelize('postgres://postgres:8888@localhost:5432/keeoshop', {
     define: {
     timestamps: false
   }
